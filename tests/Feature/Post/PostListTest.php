5658f4830f39551959ded2f149dcb30819a0594a
Aibek Baldykov <baldykov_a@yahoo.com>
2020-05-02 03:05:27 +0500

    Refactor test for list view of posts

--- tests/Feature/Post/PostListTest.php
+++ tests/Feature/Post/PostListTest.php
@@ -13,71 +13,110 @@ class PostListTest extends TestCase
 {
     use RefreshDatabase, DatabaseMigrations;
 
+    /**
+     * Fields that would be saved, asserted and validated for existence / nonexistence
+     */
+    const DATA_FIELDS_FOR_CHECK = [
+        'title',
+        'content',
+        'created_at',
+    ];
+
+    /**
+     * Creates array of data with specified field
+     * @return array
+     */
+    private function initializeCommonData()
+    {
+        $list = [];
+        foreach (self::DATA_FIELDS_FOR_CHECK as $field) {
+            $list[$field] = [];
+        }
+        return $list;
+    }
+
+    /**
+     * Saves data into array with pulling fields from the model
+     * @param $list container for saved values of previous fields
+     * @param $post model which contains specified fields
+     */
+    private function saveCommonData(&$list, $post)
+    {
+        foreach (self::DATA_FIELDS_FOR_CHECK as $field) {
+            $list[$field][] = $post->$field;
+        }
+    }
+
+    /**
+     * Asserts that response contains values in order from given list for each specified field
+     * @param $response
+     * @param $list container for previously saved field values
+     */
+    private function assertSeeTextInOrderForCommonData($response, $list)
+    {
+        foreach (self::DATA_FIELDS_FOR_CHECK as $field) {
+            $response->assertSeeTextInOrder($list[$field]);
+        }
+    }
+
+    /**
+     * Asserts that response does not contain values from given list
+     * @param $response
+     * @param $list container for previously saved field values
+     */
+    private function assertDontSeeTextInOrderForCommonData($response, $list)
+    {
+        for ($i = 0; $i < count($list[self::DATA_FIELDS_FOR_CHECK[0]]); $i++) {
+            foreach (self::DATA_FIELDS_FOR_CHECK as $field) {
+                $response->assertDontSeeText($list[$field][$i]);
+            }
+        }
+    }
+
+
     public function testPostsNewIsSortedInOrderByCreationTime()
     {
         $this->seed(\PostSeeder::class);
 
-        $postsData = [
-            'titles' => [],
-            'contents' => [],
-            'created_ats' => [],
-        ];
+        $postsData = $this->initializeCommonData();
 
         for ($i = 4; $i > 0; $i--) {
-            $art = factory(Post::class)->create([
+            $post = factory(Post::class)->create([
                 'created_at' => time() + ($i * 10),
             ]);
-            $postsData['titles'][] = $art->title;
-            $postsData['contents'][] = $art->content;
-            $postsData['created_ats'][] = $art->created_at;
+
+            $this->saveCommonData($postsData, $post);
         }
 
         $response = $this->get(route(PostController::NEW_PATH_NAME));
 
-        $response->assertSeeTextInOrder($postsData['titles']);
-        $response->assertSeeTextInOrder($postsData['contents']);
-        $response->assertSeeTextInOrder($postsData['created_ats']);
+        $this->assertSeeTextInOrderForCommonData($response, $postsData);
     }
 
     public function testPostsTopIsSortedInOrderByScore()
     {
         $this->seed(\PostSeeder::class);
 
-        $postsData = [
-            'titles' => [],
-            'contents' => [],
-            'created_ats' => [],
-        ];
+        $postsData = $this->initializeCommonData();
 
         for ($i = 10; $i > 0; $i--) {
             $post = factory(Post::class)->create([
                 'rating' => $i * 10,
             ]);
-            $postsData['titles'][] = $post->title;
-            $postsData['contents'][] = $post->content;
-            $postsData['created_ats'][] = $post->created_at;
+
+            $this->saveCommonData($postsData, $post);
         }
 
         $response = $this->get(route(PostController::TOP_PATH_NAME));
 
-        $response->assertSeeTextInOrder($postsData['titles']);
-        $response->assertSeeTextInOrder($postsData['contents']);
-        $response->assertSeeTextInOrder($postsData['created_ats']);
+        $this->assertSeeTextInOrderForCommonData($response, $postsData);
     }
 
     public function testPostsTopByDayInOrderByScoreAndDoesNotContainPreviousPosts()
     {
-        $postsData = [
-            'titles' => [],
-            'contents' => [],
-            'created_ats' => [],
-        ];
+        $postsData = $this->initializeCommonData();
 
-        $notAllowedPostsData = [
-            'titles' => [],
-            'contents' => [],
-            'created_ats' => [],
-        ];
+        $notAllowedPostsData = $this->initializeCommonData();
 
         // Posts beyond the limit
         for ($i = 10; $i > 0; $i--) {
@@ -86,9 +125,7 @@ class PostListTest extends TestCase
                 'created_at' => Carbon::now()->subHours(25)->unix(),
             ]);
 
-            $notAllowedPostsData['titles'][] = $post->title;
-            $notAllowedPostsData['contents'][] = $post->content;
-            $notAllowedPostsData['created_ats'][] = $post->created_at;
+            $this->saveCommonData($notAllowedPostsData, $post);
         }
 
         // Posts before the limit
@@ -97,39 +134,24 @@ class PostListTest extends TestCase
                 'rating' => $i * 10,
                 'created_at' => Carbon::now()->subHours(23)->unix(),
             ]);
-            $postsData['titles'][] = $post->title;
-            $postsData['contents'][] = $post->content;
-            $postsData['created_ats'][] = $post->created_at;
+
+            $this->saveCommonData($postsData, $post);
         }
 
         $response = $this->get(route(PostController::TOP_PATH_NAME, 'day'));
 
         // Posts that are beyond the limit should not be displayed
-        for ($i = 0; $i < count($notAllowedPostsData['titles']); $i++) {
-            $response->assertDontSeeText($notAllowedPostsData['titles'][$i]);
-            $response->assertDontSeeText($notAllowedPostsData['contents'][$i]);
-            $response->assertDontSeeText($notAllowedPostsData['created_ats'][$i]);
-        }
+        $this->assertDontSeeTextInOrderForCommonData($response, $notAllowedPostsData);
 
         // Posts before the limit should be displayed in order of score
-        $response->assertSeeTextInOrder($postsData['titles']);
-        $response->assertSeeTextInOrder($postsData['contents']);
-        $response->assertSeeTextInOrder($postsData['created_ats']);
+        $this->assertSeeTextInOrderForCommonData($response, $postsData);
     }
 
     public function testPostsTopByWeekInOrderByScoreAndDoesNotContainPreviousPosts()
     {
-        $postsData = [
-            'titles' => [],
-            'contents' => [],
-            'created_ats' => [],
-        ];
+        $postsData = $this->initializeCommonData();
 
-        $notAllowedPostsData = [
-            'titles' => [],
-            'contents' => [],
-            'created_ats' => [],
-        ];
+        $notAllowedPostsData = $this->initializeCommonData();
 
         // Posts beyond the limit
         for ($i = 10; $i > 0; $i--) {
@@ -138,9 +160,7 @@ class PostListTest extends TestCase
                 'created_at' => Carbon::now()->subDays(8)->unix(),
             ]);
 
-            $notAllowedPostsData['titles'][] = $post->title;
-            $notAllowedPostsData['contents'][] = $post->content;
-            $notAllowedPostsData['created_ats'][] = $post->created_at;
+            $this->saveCommonData($notAllowedPostsData, $post);
         }
 
         // Posts before the limit
@@ -149,39 +169,24 @@ class PostListTest extends TestCase
                 'rating' => $i * 10,
                 'created_at' => Carbon::now()->subDays(6)->unix(),
             ]);
-            $postsData['titles'][] = $post->title;
-            $postsData['contents'][] = $post->content;
-            $postsData['created_ats'][] = $post->created_at;
+
+            $this->saveCommonData($postsData, $post);
         }
 
         $response = $this->get(route(PostController::TOP_PATH_NAME, PostController::TOP_PATH_WEEK_STRING));
 
         // Posts that are beyond the limit should not be displayed
-        for ($i = 0; $i < count($notAllowedPostsData['titles']); $i++) {
-            $response->assertDontSeeText($notAllowedPostsData['titles'][$i]);
-            $response->assertDontSeeText($notAllowedPostsData['contents'][$i]);
-            $response->assertDontSeeText($notAllowedPostsData['created_ats'][$i]);
-        }
+        $this->assertDontSeeTextInOrderForCommonData($response, $notAllowedPostsData);
 
         // Posts before the limit should be displayed in order of score
-        $response->assertSeeTextInOrder($postsData['titles']);
-        $response->assertSeeTextInOrder($postsData['contents']);
-        $response->assertSeeTextInOrder($postsData['created_ats']);
+        $this->assertSeeTextInOrderForCommonData($response, $postsData);
     }
 
     public function testPostsTopByMonthInOrderByScoreAndDoesNotContainPreviousPosts()
     {
-        $postsData = [
-            'titles' => [],
-            'contents' => [],
-            'created_ats' => [],
-        ];
+        $postsData = $this->initializeCommonData();
 
-        $notAllowedPostsData = [
-            'titles' => [],
-            'contents' => [],
-            'created_ats' => [],
-        ];
+        $notAllowedPostsData = $this->initializeCommonData();
 
         // Posts beyond the limit
         for ($i = 10; $i > 0; $i--) {
@@ -190,9 +195,7 @@ class PostListTest extends TestCase
                 'created_at' => Carbon::now()->subDays(32)->unix(),
             ]);
 
-            $notAllowedPostsData['titles'][] = $post->title;
-            $notAllowedPostsData['contents'][] = $post->content;
-            $notAllowedPostsData['created_ats'][] = $post->created_at;
+            $this->saveCommonData($notAllowedPostsData, $post);
         }
 
         // Posts before the limit
@@ -201,39 +204,24 @@ class PostListTest extends TestCase
                 'rating' => $i * 10,
                 'created_at' => Carbon::now()->subDays(29)->unix(),
             ]);
-            $postsData['titles'][] = $post->title;
-            $postsData['contents'][] = $post->content;
-            $postsData['created_ats'][] = $post->created_at;
+
+            $this->saveCommonData($postsData, $post);
         }
 
         $response = $this->get(route(PostController::TOP_PATH_NAME, PostController::TOP_PATH_MONTH_STRING));
 
         // Posts that are beyond the limit should not be displayed
-        for ($i = 0; $i < count($notAllowedPostsData['titles']); $i++) {
-            $response->assertDontSeeText($notAllowedPostsData['titles'][$i]);
-            $response->assertDontSeeText($notAllowedPostsData['contents'][$i]);
-            $response->assertDontSeeText($notAllowedPostsData['created_ats'][$i]);
-        }
+        $this->assertDontSeeTextInOrderForCommonData($response, $notAllowedPostsData);
 
         // Posts before the limit should be displayed in order of score
-        $response->assertSeeTextInOrder($postsData['titles']);
-        $response->assertSeeTextInOrder($postsData['contents']);
-        $response->assertSeeTextInOrder($postsData['created_ats']);
+        $this->assertSeeTextInOrderForCommonData($response, $postsData);
     }
 
     public function testPostsTopByYearInOrderByScoreAndDoesNotContainPreviousPosts()
     {
-        $postsData = [
-            'titles' => [],
-            'contents' => [],
-            'created_ats' => [],
-        ];
+        $postsData = $this->initializeCommonData();
 
-        $notAllowedPostsData = [
-            'titles' => [],
-            'contents' => [],
-            'created_ats' => [],
-        ];
+        $notAllowedPostsData = $this->initializeCommonData();
 
         // Posts beyond the limit
         for ($i = 10; $i > 0; $i--) {
@@ -242,9 +230,7 @@ class PostListTest extends TestCase
                 'created_at' => Carbon::now()->subDays(366)->unix(),
             ]);
 
-            $notAllowedPostsData['titles'][] = $post->title;
-            $notAllowedPostsData['contents'][] = $post->content;
-            $notAllowedPostsData['created_ats'][] = $post->created_at;
+            $this->saveCommonData($notAllowedPostsData, $post);
         }
 
         // Posts before the limit
@@ -253,33 +239,22 @@ class PostListTest extends TestCase
                 'rating' => $i * 10,
                 'created_at' => Carbon::now()->subDays(364)->unix(),
             ]);
-            $postsData['titles'][] = $post->title;
-            $postsData['contents'][] = $post->content;
-            $postsData['created_ats'][] = $post->created_at;
+
+            $this->saveCommonData($postsData, $post);
         }
 
         $response = $this->get(route(PostController::TOP_PATH_NAME, PostController::TOP_PATH_YEAR_STRING));
 
         // Posts that are beyond the limit should not be displayed
-        for ($i = 0; $i < count($notAllowedPostsData['titles']); $i++) {
-            $response->assertDontSeeText($notAllowedPostsData['titles'][$i]);
-            $response->assertDontSeeText($notAllowedPostsData['contents'][$i]);
-            $response->assertDontSeeText($notAllowedPostsData['created_ats'][$i]);
-        }
+        $this->assertDontSeeTextInOrderForCommonData($response, $notAllowedPostsData);
 
         // Posts before the limit should be displayed in order of score
-        $response->assertSeeTextInOrder($postsData['titles']);
-        $response->assertSeeTextInOrder($postsData['contents']);
-        $response->assertSeeTextInOrder($postsData['created_ats']);
+        $this->assertSeeTextInOrderForCommonData($response, $postsData);
     }
 
     public function testPostsTopByAllTimeInOrderByScoreAndContainsAllPosts()
     {
-        $postsData = [
-            'titles' => [],
-            'contents' => [],
-            'created_ats' => [],
-        ];
+        $postsData = $this->initializeCommonData();
 
         // Posts from 2 years ago with high rating
         for ($i = 10; $i > 1; $i--) {
@@ -288,9 +263,7 @@ class PostListTest extends TestCase
                 'created_at' => Carbon::now()->subDays(700)->unix(),
             ]);
 
-            $postsData['titles'][] = $post->title;
-            $postsData['contents'][] = $post->content;
-            $postsData['created_ats'][] = $post->created_at;
+            $this->saveCommonData($postsData, $post);
         }
 
         // Current posts with low rating
@@ -298,16 +271,14 @@ class PostListTest extends TestCase
             $post = factory(Post::class)->create([
                 'rating' => $i * 10,
             ]);
-            $postsData['titles'][] = $post->title;
-            $postsData['contents'][] = $post->content;
-            $postsData['created_ats'][] = $post->created_at;
+
+            $this->saveCommonData($postsData, $post);
         }
 
         $response = $this->get(route(PostController::TOP_PATH_NAME, PostController::TOP_PATH_ALL_TIME_STRING));
 
         // All posts should be displayed in order of score
-        $response->assertSeeTextInOrder($postsData['titles']);
-        $response->assertSeeTextInOrder($postsData['contents']);
-        $response->assertSeeTextInOrder($postsData['created_ats']);
+        $this->assertSeeTextInOrderForCommonData($response, $postsData);
     }
+
 }
