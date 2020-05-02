ca36c502ea81c29ac5ddac181a82c7eb9d74f830
Aibek Baldykov <baldykov_a@yahoo.com>
2020-05-03 02:58:27 +0500

    Change auto-reformated code to the original version

--- routes/web.php
+++ routes/web.php
@@ -19,16 +19,17 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/u/{profile}', 'Web\UserController@show')->name(\App\Http\Controllers\Web\UserController::SHOW_PATH_NAME);
+Route::get('/u/{profile}', 'Web\UserController@show')
+    ->name(\App\Http\Controllers\Web\UserController::SHOW_PATH_NAME);
 
-Route::get('/a/{article}',
-    'Web\PostController@show')->name(\App\Http\Controllers\Web\PostController::SHOW_PATH_NAME);
+Route::get('/a/{article}', 'Web\PostController@show')
+    ->name(\App\Http\Controllers\Web\PostController::SHOW_PATH_NAME);
 
-Route::get('/new',
-    'Web\PostController@newArticles')->name(\App\Http\Controllers\Web\PostController::NEW_PATH_NAME);
+Route::get('/new', 'Web\PostController@newArticles')
+    ->name(\App\Http\Controllers\Web\PostController::NEW_PATH_NAME);
 
-Route::get('/top/{timeframe?}/',
-    'Web\PostController@topArticles')->name(\App\Http\Controllers\Web\PostController::TOP_PATH_NAME);
+Route::get('/top/{timeframe?}/', 'Web\PostController@topArticles')
+    ->name(\App\Http\Controllers\Web\PostController::TOP_PATH_NAME);
 
-Route::get('/{category}',
-    'Web\CategoryController@show')->name(\App\Http\Controllers\Web\CategoryController::SHOW_PATH_NAME);
+Route::get('/{category}/{timeframe?}', 'Web\CategoryController@show')
+    ->name(\App\Http\Controllers\Web\CategoryController::SHOW_PATH_NAME);
