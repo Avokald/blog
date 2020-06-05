784012b9fa77f6d2010afe203b36a35a884d5fae
Aibek Baldykov <baldykov_a@yahoo.com>
2020-06-06 01:20:46 +0500

    Add sidebar and use it with header in main index file

---
+++ resources/js/components/Sidebar/Sidebar.js
@@ -0,0 +1,39 @@
+import React from "react";
+import {connect} from 'react-redux';
+import styled from 'styled-components';
+import {Link} from "react-router-dom";
+import {absoluteToRelativePath} from "../../helpers/urlhelper";
+
+const StyledSidebar = styled.div`
+    width: 400px;
+    height: 100vh;
+`;
+
+class Sidebar extends React.Component {
+    render() {
+        let categories = this.props.categories;
+        return (
+            <StyledSidebar className="active" ref={this.sidebarRef}>
+                <ul>
+                    { categories.map((category) => {
+                        return (
+                            <li>
+                                <Link to={absoluteToRelativePath(category.link)}>
+                                    {category.title}
+                                </Link>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </StyledSidebar>
+        );
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        categories: state.categories,
+    };
+};
+
+export default connect(mapStateToProps, null)(Sidebar);
--- resources/js/index.js
+++ resources/js/index.js
@@ -4,20 +4,44 @@ import {Provider} from 'react-redux';
 import PostList from "./components/PostList";
 import PostView from "./components/PostView";
 import store from './store/index';
-import {BrowserRouter, Link, Route, Switch} from "react-router-dom";
+import {BrowserRouter, Route, Switch} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import styled from 'styled-components';
+import Sidebar from './components/Sidebar';
+import Header from "./components/Header/Header";
+
+
+const StyledApp = styled.div`
+    
+`;
+
+const StyledMain = styled.main`
+    display: flex;
+    width: 100%;
+    align-items: stratch;
+`;
+
+const StyledContent = styled.div`
+    
+`;
 
 const app = (
     <Provider store={store}>
         <BrowserRouter>
             <div>
-                <div>
-                    <Link to="/">Home</Link>
-                </div>
-                <br />
-                <Switch>
-                    <Route exact path="/" component={PostList} />
-                    <Route exact path="/a/:slugged_id" component={PostView} />
-                </Switch>
+               <Header />
+
+                <StyledMain>
+                    <Sidebar />
+
+                    <StyledContent>
+                        <Switch>
+                            <Route exact path="/" component={PostList} />
+                            <Route exact path="/a/:slugged_id" component={PostView} />
+
+                        </Switch>
+                    </StyledContent>
+                </StyledMain>
             </div>
             <div style={{ height: "1000px" }}/>
         </BrowserRouter>
