98073f24981e3f1675d1597d8ad0aaa449c76228
Aibek Baldykov <baldykov_a@yahoo.com>
2020-06-06 01:18:45 +0500

    Make reducer for categories

---
+++ resources/js/reducers/categories.js
@@ -0,0 +1,44 @@
+const initalState = [
+  {
+    "id": 1,
+    "title": "testcat",
+    "slug": "testcat",
+    "description": "testdesc",
+    "image": "https://lorempixel.com/120/120/?61201",
+    "banner": "https://lorempixel.com/640/160/?19156",
+    "created_at": "2020-05-31 18:53:32",
+    "updated_at": "2020-05-31 18:53:32",
+    "link": "http://0.0.0.0/testcat"
+  },
+  {
+    "id": 2,
+    "title": "cupiditate",
+    "slug": "cupiditate",
+    "description": "Ad cumque est temporibus suscipit sapiente molestias aut.",
+    "image": "https://lorempixel.com/120/120/?41421",
+    "banner": "https://lorempixel.com/640/160/?88199",
+    "created_at": "2020-05-31 18:53:32",
+    "updated_at": "2020-05-31 18:53:32",
+    "link": "http://0.0.0.0/cupiditate"
+  },
+  {
+    "id": 3,
+    "title": "minima",
+    "slug": "minima",
+    "description": "Aliquid necessitatibus exercitationem facere possimus id quis et necessitatibus.",
+    "image": "https://lorempixel.com/120/120/?72254",
+    "banner": "https://lorempixel.com/640/160/?89472",
+    "created_at": "2020-05-31 18:53:32",
+    "updated_at": "2020-05-31 18:53:32",
+    "link": "http://0.0.0.0/minima"
+  },
+];
+
+const reducer = (state = initalState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export default reducer;
--- resources/js/reducers/index.js
+++ resources/js/reducers/index.js
@@ -1,8 +1,10 @@
 import {combineReducers} from 'redux';
 import postsReducer from './posts';
+import categoriesReducer from './categories';
 
 const appReducer = combineReducers({
     posts: postsReducer,
+    categories: categoriesReducer,
 });
 
 export default appReducer;
