5692f5f08ce85986b77fc0361ef3e1f5ceb8571b
Aibek Baldykov <baldykov_a@yahoo.com>
2020-06-06 19:45:41 +0500

    Add action and reducer functions from opendota web project

---
+++ resources/js/reducers/reducer.js
@@ -0,0 +1,33 @@
+// https://github.com/Avokald/opendota-web/blob/master/src/reducers/reducer.js
+export default (type, initialData) => (state = {
+    loading: true,
+    data: initialData || [],
+}, action) => {
+    switch (action.type) {
+        case `REQUEST/${type}`:
+            return {
+                ...state,
+                loading: true,
+            };
+        case `OK/${type}`:
+            return {
+                ...state,
+                loading: false,
+                data: action.payload,
+                error: false,
+            };
+        case `ERROR/${type}`:
+            return {
+                ...state,
+                error: action.error || true,
+                loading: false,
+            };
+        case `QUERY/${type}`:
+            return {
+                ...state,
+                query: action.query,
+            };
+        default:
+            return state;
+    }
+};
