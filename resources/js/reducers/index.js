218fd8edddc6a57b66de18ed9fb297150ed0f826
Aibek Baldykov <baldykov_a@yahoo.com>
2020-06-06 19:49:40 +0500

    Temporary add custom reducers for single elements

--- resources/js/reducers/index.js
+++ resources/js/reducers/index.js
@@ -1,4 +1,5 @@
 import {combineReducers} from 'redux';
+import reducer from './reducer';
 import postsReducer from './posts';
 import categoriesReducer from './categories';
 import tagsReducer from './tags';
@@ -9,6 +10,10 @@ const appReducer = combineReducers({
     categories: categoriesReducer,
     tags: tagsReducer,
     users: usersReducer,
+    user: reducer('user'),
+    category: reducer('category'),
+    tag: reducer('tag'),
+    post: reducer('post'),
 });
 
 export default appReducer;
