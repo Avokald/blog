4f9f7a3119ff3543cbc542f85e96dd6abf54f54b
Aibek Baldykov <baldykov_a@yahoo.com>
2020-06-09 01:37:49 +0500

    Add action for querying single post and category and rename all to showcase their request method

--- resources/js/actions/index.js
+++ resources/js/actions/index.js
@@ -1,6 +1,10 @@
 import action from './action';
 import ApiRequest from "../routes/ApiRouter";
 
-export const fetchUserProfile = (userId) => action('user', ApiRequest.route('user', [userId]));
+export const getUserProfile = (userId) => action('user', ApiRequest.route('user', [userId]));
 
-export const fetchCategories = () => action('categories', ApiRequest.route('categories'));
+export const getCategories = () => action('categories', ApiRequest.route('categories'));
+
+export const getPost = (postId) => action('post', ApiRequest.route('post', [postId]));
+
+export const getCategory = (slug) => action('category', ApiRequest.route('category', [slug]));
