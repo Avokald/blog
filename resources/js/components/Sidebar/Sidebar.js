55833eac8d9b59cd6dd3aabd622b71fda8386f66
Aibek Baldykov <baldykov_a@yahoo.com>
2020-06-13 02:40:31 +0500

    Make sidebar fixed

--- resources/js/components/Sidebar/Sidebar.js
+++ resources/js/components/Sidebar/Sidebar.js
@@ -10,42 +10,48 @@ const StyledSidebar = styled.div`
     height: 100vh;
 `;
 
+const StyledFixedWrapper = styled.div`
+    position: fixed;
+`;
+
 class Sidebar extends React.Component {
     render() {
         let { categories = [] } = this.props;
 
         return (
             <StyledSidebar>
-                <ul className="list-group">
-                    <li className="list-group-item">
-                        <Link to={webRouter.route('frontpage')}>Popular</Link>
-                    </li>
-                    <li className="list-group-item">
-                        <Link to={webRouter.route('pageNew')}>New</Link>
-                    </li>
-                    <li className="list-group-item">
-                        <Link to={webRouter.route('categories')}>Categories</Link>
-                    </li>
-                    <li className="list-group-item">
-                        <Link to={webRouter.route('tags')}>Tags</Link>
-                    </li>
-                    <li className="list-group-item">
-                        <Link to="/users">Users</Link>
-                    </li>
+                <StyledFixedWrapper>
+                    <ul className="list-group">
+                        <li className="list-group-item">
+                            <Link to={webRouter.route('frontpage')}>Popular</Link>
+                        </li>
+                        <li className="list-group-item">
+                            <Link to={webRouter.route('pageNew')}>New</Link>
+                        </li>
+                        <li className="list-group-item">
+                            <Link to={webRouter.route('categories')}>Categories</Link>
+                        </li>
+                        <li className="list-group-item">
+                            <Link to={webRouter.route('tags')}>Tags</Link>
+                        </li>
+                        <li className="list-group-item">
+                            <Link to="/users">Users</Link>
+                        </li>
 
-                </ul>
-                <h5>Cats:</h5>
-                <ul className="list-group">
-                    { categories.map((category) => {
-                        return (
-                            <li key={category.id} className="list-group-item">
-                                <Link to={absoluteToRelativePath(category.link)}>
-                                    {category.title}
-                                </Link>
-                            </li>
-                        );
-                    })}
-                </ul>
+                    </ul>
+                    <h5>Cats:</h5>
+                    <ul className="list-group">
+                        { categories.map((category) => {
+                            return (
+                                <li key={category.id} className="list-group-item">
+                                    <Link to={absoluteToRelativePath(category.link)}>
+                                        {category.title}
+                                    </Link>
+                                </li>
+                            );
+                        })}
+                    </ul>
+                </StyledFixedWrapper>
             </StyledSidebar>
         );
     }
